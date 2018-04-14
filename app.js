14ced9474b3a61ef9bfc2e624283409a732271d3
Arun Jeevanantham <itsmearun98@gmail.com>
2018-04-14 19:22:23 -0400

    Updated Socket stuff

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var spectator = io.of('/spectator');
+var player = io.of('/player');
+
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:' + 3000);
+});
+
+player.on('connection', function(socket){
+	console.log("Player Connected");
+  socket.on('chat message', function(msg){
+    player.emit('chat message', msg);
+  });
+});
+
+spectator.on('connection', function(socket){
+	console.log("Spectator Connected");
+  socket.on('chat message', function(msg){
+    spectator.emit('chat message', msg);
+  });
 });
